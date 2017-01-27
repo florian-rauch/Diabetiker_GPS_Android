e3e8b681a1df002c0737266bb7ee51954de18b2d
Florian Rauchenzauner <florian.rauchenzauner@hotmail.com>
2017-01-27 13:15:15 +0100

    Position änderungs event handler hinzugefügt

--- app/src/main/java/com/example/flori/diabetiker_gps/MainActivity.java
+++ app/src/main/java/com/example/flori/diabetiker_gps/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.flori.diabetiker_gps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private static LocationManager locMan = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locMan = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        ((Button) findViewById(R.id.button_anzeigen)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAnzeigenbtnclicked();
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+    }
+
+    private void onAnzeigenbtnclicked() {
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location!=null){
+            double la = location.getLatitude();
+            double lo = location.getLongitude();
+            Date d = new Date();
+
+            Positions p=new Positions(
+                    String.format("%.4f",lo),
+                    String.format("%.4f",la),
+                    android.text.format.DateFormat.format("DD.MM.YYYY",d.getTime()).toString(),
+                    android.text.format.DateFormat.format("HH:mm",d.getTime()).toString());
+
+
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+    @Override
+    public void onProviderEnabled(String s) {}
+
+    @Override
+    public void onProviderDisabled(String s) {}
 }
