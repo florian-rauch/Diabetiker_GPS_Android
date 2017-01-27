961f571cc858a6ef1e63fe4e2f0cb4c74e40d015
Florian Rauchenzauner <florian.rauchenzauner@hotmail.com>
2017-01-27 12:22:43 +0100

    Positions Table hinzugefügt

---
+++ app/src/main/java/com/example/flori/diabetiker_gps/Positions.java
@@ -0,0 +1,8 @@
+package com.example.flori.diabetiker_gps;
+
+/**
+ * Created by flori on 27.01.2017.
+ */
+
+public class Positions {
+}
---
+++ app/src/main/java/com/example/flori/diabetiker_gps/PositionsTbl.java
@@ -0,0 +1,29 @@
+package com.example.flori.diabetiker_gps;
+
+/**
+ * Created by flori on 27.01.2017.
+ */
+
+public class PositionsTbl {
+    public static final String TABLE_NAME="Positions";
+
+    public static final String Longitude="Longitude";
+    public static final String Latitude="Latitude";
+    public static final String MyDate="MyDate";
+    public static final String Time="Time";
+
+    public static final String[] ALL_COLUMNS=new String[]{Longitude,Latitude,MyDate,Time};
+    public static final String SQL_DROP="DROP TABLE IF EXISTS "+ TABLE_NAME;
+    public static final String SQL_CREATE="CREATE TABLE "+TABLE_NAME+"("+
+            Longitude+" TEXT NOT NULL, "+
+            Latitude+" TEXT NOT NULL, "+
+            MyDate+" TEXT NOT NULL, "+
+            Time+" TEXT NOT NULL)";
+    public static final String STMT_DELETE="DELETE FROM "+TABLE_NAME;
+    public static final String STMT_INSERT="INSERT INTO "+TABLE_NAME+
+            "( "+Longitude+", "+Latitude+", "+MyDate+ ", "+Time+") VALUES(?,?,?,?)";
+
+
+
+
+}
